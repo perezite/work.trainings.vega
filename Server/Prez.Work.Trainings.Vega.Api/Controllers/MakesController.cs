da12f7e3263810025c97b92e53faee946f452c61
Prez <simulo2000@gmail.com>
2022-09-17 18:25:42 +0200

    feat: Add makes controller wip

---
+++ Server/Prez.Work.Trainings.Vega.Api/Controllers/MakesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Prez.Work.Trainings.Vega.Api.Models;
+using Prez.Work.Trainings.Vega.Api.Persistence;
+
+namespace Prez.Work.Trainings.Vega.Api.Controllers
+{
+    public class MakesController : Controller
+    {
+        private readonly VegaDbContext context;
+
+        public MakesController(VegaDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<Make>> GetMakes()
+        {
+            return await context.Makes.Include(m => m.Models).ToListAsync(); 
+        }
+    }
+}
