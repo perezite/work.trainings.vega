581e7056a0b3dad3867164ad610a0e707e0fa2ad
Prez <simulo2000@gmail.com>
2022-09-17 19:05:02 +0200

    feat: Add seed features migration

---
+++ Server/Prez.Work.Trainings.Vega.Api/Migrations/20220917165416_SeedFeatures.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prez.Work.Trainings.Vega.Api.Migrations
+{
+    public partial class SeedFeatures : Migration
+    {
+        private static List<string> features = new() { "Air Conditioning", "Antilock Braking System", "Rear View Camera" };
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            foreach(var feature in features)
+            {
+                migrationBuilder.Sql($"INSERT INTO Features(Name) VALUES('{feature}')");
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            foreach (var feature in features)
+            {
+                migrationBuilder.Sql($"DELETE FROM Features where Name = '{feature}'");
+            }
+        }
+    }
+}
