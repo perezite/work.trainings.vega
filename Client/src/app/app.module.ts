539abef72daa957814879b48fa8185b343e3c67d
Prez <simulo2000@gmail.com>
2022-12-11 00:09:00 +0100

    lecture 35: Add basic layout and navbar

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
+import { NavigationComponent } from './components/app/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VehicleFormComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
