35af56710f67f521037e7216869821a56237e41a
Prez <simulo2000@gmail.com>
2023-03-12 16:47:11 +0100

    adding mobile sidebar closer wip

--- Client/src/app/icons/icons.module.ts
+++ Client/src/app/icons/icons.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { TablerIconsModule } from 'angular-tabler-icons';
-import { IconCamera, IconHeart, IconBrandGithub, IconDashboard, IconCar } from 'angular-tabler-icons/icons';
+import { IconCamera, IconHeart, IconBrandGithub, IconDashboard, IconCar, IconCircleArrowLeft } from 'angular-tabler-icons/icons';
 
 // Select some icons (use an object, not an array)
 const icons = {
@@ -9,7 +9,8 @@ const icons = {
   IconHeart,
   IconBrandGithub,
   IconDashboard,
-  IconCar
+  IconCar,
+  IconCircleArrowLeft
 };
 
 @NgModule({
