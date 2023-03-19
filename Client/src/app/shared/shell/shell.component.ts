676f6c22557f60e42bb50172d1bb0b61ee45fca2
Prez <simulo2000@gmail.com>
2023-03-19 15:26:53 +0100

    expand sidebar on desktop

--- Client/src/app/shared/shell/shell.component.ts
+++ Client/src/app/shared/shell/shell.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss']
 })
-export class ShellComponent {
+export class ShellComponent implements OnInit {
   sidebarExpanded = false;
+
+  ngOnInit(): void {
+    const isMobile = window.matchMedia("screen and (max-width: 576px)").matches;
+    if(!isMobile)
+      this.sidebarExpanded = true;
+  }
 
   expandSidebar(expand: boolean) {
     this.sidebarExpanded = !this.sidebarExpanded;
   }
+
+
 }
