e537029b3479f9760cf6ec5c2216d34eb94eca8b
Prez <simulo2000@gmail.com>
2023-03-19 18:07:51 +0100

    add vehicle from route

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavbarDemoComponent } from './shared/shell/navbar/demo/navbar-demo.component';
 import { SandboxComponent } from './sandbox/sandbox.component';
 import { SidebarDemoComponent } from './shared/shell/sidebar/demo/sidebar-demo.component';
+import { VehicleFormComponent } from './vehicles/vehicle-form.component';
 
 const routes: Routes = [
   { path: 'navbar-demo', component: NavbarDemoComponent, data: { fullscreen: 'true' } },
   { path: 'sidebar-demo', component: SidebarDemoComponent, data: { fullscreen: 'true' } },
+  { path: 'new-vehicle', component: VehicleFormComponent, data: { fullscreen: 'false' } },
   { path: '**', component: SandboxComponent }
 ];
 
